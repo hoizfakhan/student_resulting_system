b19634546f7ed50b9ad2f94606f78d18ec6bac13
hasibullah hozifa <hasibullahhozifa@gmail.com>
2024-06-29 06:32:01 +0430

    some changes

--- resources/js/Pages/SuperAdmin/teacher/Create.jsx
+++ resources/js/Pages/SuperAdmin/teacher/Create.jsx
@@ -166,7 +166,6 @@ export default function Create({auth}){
                               </button>
                            </div>
                         </form>
-
       </div>
      </div>
      </div>
