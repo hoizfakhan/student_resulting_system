068fbda50382fd89319bddc99bdea8cd25e3073f
imran-arman <163504550+imran-arman@users.noreply.github.com>
2024-06-30 23:00:21 +0430

    Update the ApplicationLogo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -4,7 +4,7 @@ export default function ApplicationLogo(props) {
     return (
         <img
             {...props} // Preserve the props for styling
-            src="/images/ApplicationLogo.jpg" // Update this to the path of your new image
+            src="/images/ApplicationLogo.png" // Update this to the path of your new image
             alt="Application Logo"
             className="w-20 h-20 " // Ensure the image has appropriate styling
         />
